0dd2d1ac43eb7803c019c1963f7d4710f43e0131
DuduNeri <nerimartinseduardo@gmail.com>
2024-11-05 21:05:42 -0300

    first commit

---
+++ cronometro/src/App.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import './App.css';
+import React from 'react';
+ function App() {
+
+    render(App){
+      <div>
+        
+      </div>
+    }
+ }
+
+export default App;
